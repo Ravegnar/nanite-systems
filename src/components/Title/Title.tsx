d3ad3a179fedbb4afa9d1c5a93c7101da1a7eca5
Ravegnar <Asantir@email.cz>
2023-10-03 23:58:46 +0200

    Title A1E3

--- src/components/Title/Title.tsx
+++ src/components/Title/Title.tsx
@@ -15,6 +15,7 @@ const Title = ({
 
   const yDirection = direction[0]
   const xDirection = direction[1]
+  const isRight = xDirection === "right"
   
   const setElement = (element: any, types: string, values: string, timeout: number = 0) => {
     let typesData: string[] = types.split(",")
@@ -58,8 +59,8 @@ const Title = ({
     ctx.lineWidth = 2;
     // Start a new Path
     ctx.beginPath();
-    ctx.moveTo(0, 0);
-    ctx.lineTo(titleOffset, titleOffset);
+    ctx.moveTo((isRight ? 0 : titleOffset), 0);
+    ctx.lineTo((isRight ? titleOffset : 0), titleOffset);
 
     // Draw the Path
     ctx.strokeStyle = "white";
@@ -67,7 +68,7 @@ const Title = ({
 
     const titleWidth = titleText.current.getBoundingClientRect().width
     setElement(titleBody, "width", "0px")
-    setElement(titleBody, "width", titleWidth +"px", 200)
+    setElement(titleBody, "width", titleWidth +"px", 150)
   }, [showTooltip]);
 
   const handleMouseEnter = () => {
@@ -90,26 +91,26 @@ const Title = ({
           <Icon
             type="ArrangeSendBackward"
             size={16}
-            className={`absolute top-full ${xDirection}-full flex-nowrapXXX text-white`}
+            className={`absolute top-full ${isRight ? "left" : "[transform:rotateY(0.5turn)] right"}-full flex-nowrapXXX text-white `}
           />
           <canvas
             ref={canvas}
             width={titleOffset + "px"}
             height={titleOffset + "px"}
-            className={`absolute inline-block w-auto -bottom-[13px] -${xDirection}-[13px] translate-y-full translate-x-full flex-nowrapXXX text-white`}
+            className={`absolute inline-block w-auto -bottom-[13px] -${xDirection}-[13px] translate-y-full ${isRight ? "translate-x-full" : "-translate-x-full"} flex-nowrapXXX text-white`}
           ></canvas>
           <div
             ref={titleBody}
             style={{
               bottom: -(titleOffset - 28 + 13) + "px",
-              right: xDirection === "right" ? -(titleOffset - 1) + "px" : "none",
+              right: isRight ? -(titleOffset - 1) + "px" : "none",
               left: xDirection === "left" ? -(titleOffset - 1) + "px" : "none",
             }}
-            className={`absolute w-0 flex h-7 whitespace-nowrap bg-cyan-600/50 text-black translate-y-full translate-x-full
-              skew-x-[45deg] border-l-[10px] border-b-[2px] border-white animate-blinkX2X transition-all transform duration-75 ease-in 
+            className={`absolute w-0 flex h-7 whitespace-nowrap bg-cyan-600/50 text-black translate-y-full ${isRight ? "translate-x-full" : "-translate-x-full"}
+              skew-x-[${isRight ? "" : "-"}45deg] border-${isRight ? "l" : "r"}-[10px] border-b-[2px] border-white animate-blinkX2X transition-all transform duration-75 ease-in 
             `}
           >
-            <div ref={titleText} className="flex items-center text-white animate-blinkV2 font-medium px-4" style={{ transform: "skewX(-45deg)" }}>
+            <div ref={titleText} className="flex items-center text-white animate-blinkV2 font-medium px-6" style={{ transform: `skewX(${isRight ? "-" : ""}45deg)` }}>
               {text}
             </div>
           </div>
